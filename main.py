05f4b8d74b1269107a34da578aba11f58c6232b2
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2025-06-15 11:30:11 +0200

    update

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 import os
 import importlib.util
 import multiprocessing
-from flask import Flask, render_template, Blueprint
+from flask import Flask, render_template
+from jinja2 import ChoiceLoader, FileSystemLoader
 
 app = Flask(__name__)
 
 PACKAGE_DIR = os.path.join(os.path.dirname(__file__), 'packages')
 
-# Dynamically register blueprints for each service in packages
-def make_service_index():
-    def service_index():
-        return render_template('index.html')
-    return service_index
+# Dynamically add all package template folders to Jinja loader
+extra_template_folders = [os.path.join(PACKAGE_DIR, pkg, 'templates')
+    for pkg in os.listdir(PACKAGE_DIR)
+    if os.path.isdir(os.path.join(PACKAGE_DIR, pkg, 'templates'))]
 
-for service in os.listdir(PACKAGE_DIR):
-    service_path = os.path.join(PACKAGE_DIR, service)
-    service_template_dir = os.path.join(service_path, 'templates')
-    service_index_html = os.path.join(service_template_dir, 'index.html')
-    if os.path.isdir(service_path) and os.path.exists(service_index_html):
-        bp = Blueprint(
-            service,
-            __name__ + '_' + service,  # unique name for each blueprint
-            template_folder=service_template_dir,
-            url_prefix=f'/{service}'
-        )
-        bp.route('/')(make_service_index())
-        app.register_blueprint(bp)
+# Always include the main templates folder
+main_templates = os.path.join(os.path.dirname(__file__), 'templates')
+app.jinja_loader = ChoiceLoader([
+    FileSystemLoader([main_templates] + extra_template_folders)
+])
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# Dynamically create a route for each package that has a templates/index.html
+for pkg in os.listdir(PACKAGE_DIR):
+    template_path = os.path.join(PACKAGE_DIR, pkg, 'templates', 'index.html')
+    if os.path.exists(template_path):
+        route = f'/{pkg}'
+        def make_view(pkg_name):
+            def view():
+                # Render the correct index.html for the package by referencing it as 'index.html' (since it's in the extra_template_folders)
+                return render_template('index.html')
+            return view
+        app.add_url_rule(route, f'{pkg}_index', make_view(pkg))
 
 if __name__ == '__main__':
     processes = []
